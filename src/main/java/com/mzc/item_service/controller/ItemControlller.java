3137c91a36853aa9e3e5862651140da95a254d1b
kimsinok0714 <javaenjoy0714@gmail.com>
2024-08-14 07:04:02 +0900

    update v2

--- src/main/java/com/mzc/item_service/controller/ItemControlller.java
+++ src/main/java/com/mzc/item_service/controller/ItemControlller.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mzc.item_service.dto.ItemDto;
 import com.mzc.item_service.dto.ResponseDto;
-import com.mzc.item_service.exception.ApiException;
 import com.mzc.item_service.service.ItemService;
 import com.mzc.item_service.valid.ItemTypeValid;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +23,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
+
+@OpenAPIDefinition(info = @Info(title = "Item Service API", description = "물품 처리 요청 API", version = "v1"))
 @RestController
 @RequestMapping(value = "api/v1/items")
 @Slf4j
@@ -28,8 +35,13 @@ public class ItemControlller {
 
     private final ItemService itemService;
 
-   
+    
     // Validation Check
+    @Operation(summary = "아이템 등록 요청", description = "아이템 등록을 처리한다.", tags = {"createItem"})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "SUCCESS"),
+        @ApiResponse(responseCode = "501", description = "ApiException")
+    })    
     @PostMapping(value = "/{itemType}")
     public ResponseEntity<ResponseDto> createItem(@Valid @RequestBody ItemDto itemDto, @ItemTypeValid @PathVariable(value = "itemType") String itemType) throws Exception {
            
--- src/main/java/com/mzc/item_service/dto/ItemDto.java
+++ src/main/java/com/mzc/item_service/dto/ItemDto.java
@@ -1,7 +1,6 @@
 package com.mzc.item_service.dto;
 
-import com.mzc.item_service.dto.constant.ItemType;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -18,14 +17,27 @@ import lombok.Data;
 
 @Data
 public class ItemDto {
+    @Schema(description = "물품ID", example = "TESTID")
     @NotBlank(message = "ID is a required field.")
     @Size(max = 10, message = "ID can be up to 10 characters long.")
-    private String id;   
+    private String id;  
+
+    @Schema(description = "물품명", example = "과일")
     private String name;
+    
+    @Schema(description = "물품설명", example = "물품설명테스트")
     private String description;
+    
+    @Schema(description = "물품개수", example = "100")
     @Positive(message = "Only positive numbers are allowed.")
     private long count;
-    private String regDate;
-    private String upDate;
+    
+    @Schema(hidden= true)
+    private String regDate;    
+    
+    @Schema(hidden= true)
+    private String updDate;
+    
+    @Schema(description = "물품유형", example = "F: 음식, C: 의류")
     private String itemType;
 }
